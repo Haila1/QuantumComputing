da5712e8452dcab5f4d8941c833db5e138fcdde4
Haila1 <128238595+Haila1@users.noreply.github.com>
2023-08-02 05:13:36 +0300

    Add files via upload

---
+++ Entanglement/Teleportation.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  1 20:25:45 2023
+
+@author: ha
+"""
+
+from qiskit import qiskit
+from qiskit import *
+from qiskit import QuantumCircuit, transpile, Aer, IBMQ
+from qiskit.tools.jupyter import *
+from qiskit.visualization import *
+from qiskit.providers.aer import QasmSimulator
+from math import *
+from qiskit.quantum_info import *
+
+qc = QuantumRegister(3)
+cx = ClassicalRegister(1) 
+cz = ClassicalRegister(1) 
+teleportation = QuantumCircuit(qc,cx,cz)   
+
+
+psi = random_statevector(2) 
+teleportation.initialize(psi  , 0) 
+plot_bloch_multivector(psi)
+teleportation.barrier()
+
+
+def bellState():
+    teleportation.h(1)
+    teleportation.cx(1,2)
+    teleportation.barrier()
+
+def sara():
+    teleportation.cx(0, 1)
+    teleportation.h(0)
+    teleportation.barrier()
+    teleportation.measure(0, cz)
+    teleportation.measure(1, cx)
+
+def khalid():
+    teleportation.x(2).c_if(cx, 1) 
+    teleportation.z(2).c_if(cz, 1)
+    
+   
+bellState()
+sara()
+khalid()
+
+teleportation.draw('mpl')
+job = execute(teleportation, Aer.get_backend('statevector_simulator'))
+output = job.result().get_statevector()
+plot_bloch_multivector(output)
+
