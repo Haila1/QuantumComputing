173cc5727736da35f50dcf8926eb3cf93b7e2349
Haila1 <128238595+Haila1@users.noreply.github.com>
2023-08-02 05:21:36 +0300

    Teleportation.py

--- Entanglement/Teleportation.py
+++ Entanglement/Teleportation.py
@@ -20,25 +20,25 @@ cx = ClassicalRegister(1)
 cz = ClassicalRegister(1) 
 teleportation = QuantumCircuit(qc,cx,cz)   
 
-
+# step 0: psi and bell state 
 psi = random_statevector(2) 
 teleportation.initialize(psi  , 0) 
 plot_bloch_multivector(psi)
 teleportation.barrier()
 
-
 def bellState():
     teleportation.h(1)
     teleportation.cx(1,2)
     teleportation.barrier()
-
+#step 2.1: CNOT then H the psi and Bell state
 def sara():
     teleportation.cx(0, 1)
     teleportation.h(0)
     teleportation.barrier()
+    # step 2.2: measure 
     teleportation.measure(0, cz)
     teleportation.measure(1, cx)
-
+#step 3: X and Z gates, (X if cx==1) (Z if cz ==1) 
 def khalid():
     teleportation.x(2).c_if(cx, 1) 
     teleportation.z(2).c_if(cz, 1)
